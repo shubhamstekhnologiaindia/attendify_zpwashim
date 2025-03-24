b4899fbdfc074fddd083d28ad8634ae506bf22cb
shubhamstekhnologiaindia <shubham.sanap@tekhnologiaindia.com>
2025-03-24 16:02:20 +0530

    merged code

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Password@0157',
+    password: '',
     database: 'attendify_zp_washim',
     waitForConnections: true,
     connectionLimit: 10,
