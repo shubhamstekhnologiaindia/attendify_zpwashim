f0d9c21e21196b970a02cdef45538d6f86cc64c3
shubhamstekhnologiaindia <shubham.sanap@tekhnologiaindia.com>
2025-04-08 15:55:24 +0530

    otp code add in registration

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
- 
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import AuthRoute from "./src/modules/Auth/routes/AuthRoute.js";
-// import dropdownRoute from "./src/modules/Auth/routes/dropdownRoute.js";
-// import attendanceRoutes from "./src/modules/Users/routes/attendanceRoutes.js";
-// import hodRoutes from "./src/modules/Hod/routes/hodRoutes.js";
-// import GrRoutes from "./src/modules/Admin/routes/GrRoutes.js";
-// import UserRoutes from "./src/modules/Users/routes/userRoutes.js";
-// import notificationRoutes from "./src/modules/BirthdayNotification/routes/notification.routes.js";
-
-// // import notificationRoutes from "./src/modules/BirthdayNotification/routes/notification.routes.js";
-
-
- 
-// const app = express();
- 
-// app.use(cors());
-// app.use(express.json());
- 
-// app.use('/api/auth', AuthRoute);
-// app.use('/api', dropdownRoute, hodRoutes,attendanceRoutes,GrRoutes,UserRoutes,notificationRoutes);
-
-// const PORT = process.env.PORT || 3008;
- 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// })
-
-
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
@@ -47,17 +14,18 @@ import SendmsgRoute from "./src/modules/Admin/routes/msgRoute.js";
 import  HqRoutes from "./src/modules/Admin/routes/hqRoute.js";
 
 import UserController from "./src/modules/Users/routes/userRoute.js";
+import otpRoute from "./src/modules/Users/routes/otpRoute.js";
+
 import notificationRoutes from "./src/modules/BirthdayNotification/routes/notificationRoute.js";
 
 const app = express();
  
 app.use(cors());
 app.use(express.json());
-// Serve static files from 'uploads' directory
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', AuthRoute);
-app.use('/api', masterDataRoute, hodRoutes,attendanceRoute,GrRoutes,SendmsgRoute,HqRoutes,notificationRoutes,UserController);
+app.use('/api', masterDataRoute, hodRoutes,attendanceRoute,GrRoutes,SendmsgRoute,HqRoutes,notificationRoutes,UserController,otpRoute);
 
 const PORT = 3001
  
