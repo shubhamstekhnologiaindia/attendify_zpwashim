7d4e1f5372af0294ed3415f5cfc38ed91aed5152
Kajalptl77 <kajal.patil@tekhnologiaindia.com>
2025-04-08 15:03:06 +0530

    upcomming birthday api

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,7 @@ import GrRoutes from "./src/modules/Admin/routes/GrRoute.js";
 import SendmsgRoute from "./src/modules/Admin/routes/msgRoute.js";
 import  HqRoutes from "./src/modules/Admin/routes/hqRoute.js";
 import  holidayRoute from "./src/modules/WebDashboard/routes/holidayRoute.js";
+import  birthdayRoute from "./src/modules/WebDashboard/routes/birthdayRoute.js";
 
 
 import notificationRoutes from "./src/modules/BirthdayNotification/routes/notificationRoute.js";
@@ -58,7 +59,7 @@ app.use(express.json());
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', AuthRoute);
-app.use('/api', masterDataRoute, hodRoutes,attendanceRoute,GrRoutes,SendmsgRoute,HqRoutes,notificationRoutes, holidayRoute);
+app.use('/api', masterDataRoute, hodRoutes,attendanceRoute,GrRoutes,SendmsgRoute,HqRoutes,notificationRoutes, holidayRoute, birthdayRoute);
 
 const PORT = 3001
  
---
+++ backend/src/modules/WebDashboard/controllers/birthdayController.js
@@ -0,0 +1,32 @@
+import { BirthdayService } from "../services/birthdayService.js";
+import moment from "moment-timezone";
+
+export const BirthdayController = {
+    showTodaysBirthdays: async (req, res) => {
+        try {
+            const birthdays = await BirthdayService.getTodaysBirthdays();
+
+            if (!birthdays.length) {
+                return res.status(200).json({
+                    success: true,
+                    today: moment().format("YYYY-MM-DD"),
+                    message: "No birthdays today",
+                    data: []
+                });
+            }
+
+            res.status(200).json({
+                success: true,
+                today: moment().format("YYYY-MM-DD"),
+                birthdays
+            });
+
+        } catch (error) {
+            console.error("Error fetching today's birthdays:", error);
+            res.status(500).json({
+                success: false,
+                message: "Failed to fetch birthdays"
+            });
+        }
+    }
+};
