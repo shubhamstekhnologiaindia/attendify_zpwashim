3ec9ac9a9e8f64c5d561933c350e24110ed9fdb2
Kajalptl77 <kajal.patil@tekhnologiaindia.com>
2025-04-23 19:36:58 +0530

    show proper cader

--- backend/src/modules/sanstha/controllers/sansthaController.js
+++ backend/src/modules/sanstha/controllers/sansthaController.js
@@ -38,7 +38,7 @@ export const SansthaController = {
           await SansthaService.updateSanstha({
             loc_id,
             loc_name_marathi,
-            loc_name_eng,
+            loc_name_eng: loc_name_eng ? loc_name_eng : '' ,
             dept_id
           });
     
