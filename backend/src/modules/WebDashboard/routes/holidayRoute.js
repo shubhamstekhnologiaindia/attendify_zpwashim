44a1d6a1ad58481ad111f28074a9d2b00649963c
Kajalptl77 <kajal.patil@tekhnologiaindia.com>
2025-04-26 12:59:19 +0530

    SANAP PUL WITH UPDATED CODE OF RADIUS

--- backend/src/modules/WebDashboard/routes/holidayRoute.js
+++ backend/src/modules/WebDashboard/routes/holidayRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/upcomming-holiday", authMiddleware,HolidayController.showHolidays); 
 
-router.put('/update_radius', HolidayController.updateRadius);
-router.get('/show_radius', HolidayController.getRadiusWeb);
+router.put('/update_radius',authMiddleware ,HolidayController.updateRadius);
+router.get('/show_radius',authMiddleware,HolidayController.getRadiusWeb);
 
 export default router;
