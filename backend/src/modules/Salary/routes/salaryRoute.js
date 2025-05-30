864bbfb595ca9614086860f60baea8b480b21361
shubhamstekhnologiaindia <shubham.sanap@tekhnologiaindia.com>
2025-05-30 18:07:32 +0530

    change code in salay route

--- backend/src/modules/Salary/routes/salaryRoute.js
+++ backend/src/modules/Salary/routes/salaryRoute.js
@@ -5,25 +5,25 @@ import {SalaryController} from "../controllers/salaryController.js";
 
 const router = express.Router();
 
-router.post('/Save_Users_For_Salary_Slip_Permission',authMiddleware.SalaryController.saveSalarySlipPermission);
-router.put('/Update_Users_For_Salary_Slip_Permission', authMiddleware.SalaryController.updateSalarySlipPermission);
-router.delete('/Delete_Users_For_Salary_Slip_Permission/:salary_slip_per_id', authMiddleware.SalaryController.deleteSalarySlipPermission);
+router.post('/Save_Users_For_Salary_Slip_Permission',authMiddleware,SalaryController.saveSalarySlipPermission);
+router.put('/Update_Users_For_Salary_Slip_Permission', authMiddleware,SalaryController.updateSalarySlipPermission);
+router.delete('/Delete_Users_For_Salary_Slip_Permission/:salary_slip_per_id', authMiddleware,SalaryController.deleteSalarySlipPermission);
 
-router.get('/checking_salary_slip_per', authMiddleware.SalaryController.checking_salary_slip_per);
+router.get('/checking_salary_slip_per', authMiddleware,SalaryController.checking_salary_slip_per);
 
-router.post('/salary-slips-store', authMiddleware.SalaryController.storeSalarySlipRequest);
-router.get('/salary-slip-permitters', authMiddleware.SalaryController.listSalarySlipPermissions);
+router.post('/salary-slips-store', authMiddleware,SalaryController.storeSalarySlipRequest);
+router.get('/salary-slip-permitters', authMiddleware,SalaryController.listSalarySlipPermissions);
 
 
 const upload = multer({ storage: multer.memoryStorage() });
-router.post('/Upload_Salary_Slip', authMiddleware.upload.any(), SalaryController.uploadSalarySlipToAzure);
+router.post('/Upload_Salary_Slip', authMiddleware,upload.any(), SalaryController.uploadSalarySlipToAzure);
 
-router.get('/FetchUsersForSalarySlip', authMiddleware.authMiddleware,SalaryController.getSalarySlips);
+router.get('/FetchUsersForSalarySlip', authMiddleware,SalaryController.getSalarySlips);
 
-router.post('/salary-slips-request-history', authMiddleware.SalaryController.listSalarySlipsBySender);
-router.get('/Salary-Heads',authMiddleware.SalaryController.getListOfSalaryHeads);
+router.post('/salary-slips-request-history', authMiddleware,SalaryController.listSalarySlipsBySender);
+router.get('/Salary-Heads',authMiddleware,SalaryController.getListOfSalaryHeads);
 
-router.put('/salary-slip/request-reject', authMiddleware.SalaryController.rejectSalarySlipRequest);
+router.put('/salary-slip/request-reject', authMiddleware,SalaryController.rejectSalarySlipRequest);
 
 export default router;
 
--- backend/utils/database.js
+++ backend/utils/database.js
@@ -5,8 +5,8 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS|| "#BMWrootsql@010",
-    database: process.env.DB_NAME || 'attendify_zp_washim_new',
+    password: process.env.DB_PASS|| "",
+    database: process.env.DB_NAME || 'attends-zp-washim-uat',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
